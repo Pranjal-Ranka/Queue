6870e12d1a40eb2974864f1f6f767212e796f5e7
Pranjal Ranka <pranjal.ranka@techniatranscat.com>
2019-01-08 01:14:37 +0530

    Hello World Commit

---
+++ Queue.cpp
@@ -0,0 +1,15 @@
+// Queue.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    cout<<"Hello World";
+	getchar();
+    return 0;
+}
+
